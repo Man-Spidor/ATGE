d305851b5b6a8968271ddf2e6aeaf44f70bbab0a
Man-Spidor <ocampo.mjesus@gmail.com>
2025-03-01 16:31:01 -0600

    Create Build.lua

---
+++ Build.lua
@@ -0,0 +1,15 @@
+-- premake5.lua
+workspace "ATGE"
+   architecture "x64"
+   configurations { "Debug", "Release", "Dist" }
+   startproject "Game"
+
+   -- Workspace-wide build options for MSVC
+   filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+-- REMOVE GROUPS
+include "ATGE/Build-ATGE.lua"
+include "Game/Build-Game.lua"
